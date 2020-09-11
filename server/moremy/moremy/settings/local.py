501a3b1f53abf13b6a9da7e57900e6bddc77488e
jorge-jcc <JorgeCrdenas@hotmail.com>
2020-09-11 16:02:16 -0500

    Configuraciones iniciales Django

---
+++ server/moremy/moremy/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+  "default": {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": BASE_DIR.child("db.sqlite3"),
+  }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR.child("static")]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.child("media")
